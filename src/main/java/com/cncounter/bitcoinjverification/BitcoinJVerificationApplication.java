af28f7da051f1ca6fae4f820a13a0d472cec1a69
renfufei <renfufei@qq.com>
2023-11-26 18:49:15 +0800

    处理默认没有数据源配置时的启动报错

--- src/main/java/com/cncounter/bitcoinjverification/BitcoinJVerificationApplication.java
+++ src/main/java/com/cncounter/bitcoinjverification/BitcoinJVerificationApplication.java
@@ -2,12 +2,15 @@ package com.cncounter.bitcoinjverification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {DataSourceAutoConfiguration.class}
+)
 public class BitcoinJVerificationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BitcoinJVerificationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BitcoinJVerificationApplication.class, args);
+    }
 
 }
